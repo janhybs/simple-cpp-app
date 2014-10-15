2452951cac68d23f2e4ddf51d838a8a53b114a23
x3mSpeedy <x3mSpeedy@gmail.com>
2014-10-15 13:05:06 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,17 +1,20 @@
 #include <iostream>
 
 main (int c, char* argv[]) {
+    
+    std::cout << "Total args: " << c << endl << endl;
+      
     if (c == 1) {
-      std::cout << "Hello World!";
+      std::cout << "Hello World!" << endl;
     } else
     if (c == 2) {
-      std::cout << "Hello World 2nd time!";
+      std::cout << "Hello World 2nd time!" << endl;
     } else
     if (c == 3) {
-      std::cout << "Hi!";
+      std::cout << "Hi!" << endl;
     } else
     if (c == 4) {
-      std::cout << "Hi again!";
+      std::cout << "Hi again!" << endl;
     }
       
     return 0;
