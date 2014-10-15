11dfb38c37e42e27262c2b892ed803e7e66fb65e
x3mSpeedy <x3mSpeedy@gmail.com>
2014-10-15 13:10:20 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -2,19 +2,19 @@
 
 main (int c, char* argv[]) {
     
-    std::cout << "Total args: " << c << endl << endl;
+    std::cout << "Total args: " << c <<  std::endl <<  std::endl;
       
     if (c == 1) {
-      std::cout << "Hello World!" << endl;
+      std::cout << "Hello World!" <<  std::endl;
     } else
     if (c == 2) {
-      std::cout << "Hello World 2nd time!" << endl;
+      std::cout << "Hello World 2nd time!" <<  std::endl;
     } else
     if (c == 3) {
-      std::cout << "Hi!" << endl;
+      std::cout << "Hi!" <<  std::endl;
     } else
     if (c == 4) {
-      std::cout << "Hi again!" << endl;
+      std::cout << "Hi again!" <<  std::endl;
     }
       
     return 0;
