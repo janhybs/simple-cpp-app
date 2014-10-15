4a594c88fa055e9c8675f765c4ab27815e30f5b4
x3mSpeedy <x3mSpeedy@gmail.com>
2014-10-15 11:27:58 +0200

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,18 @@
+#include <iostream.h>
+
+main (int c, char* argv[]) {
+    if (c == 1) {
+      cout << "Hello World!";
+    } else
+    if (c == 2) {
+      cout << "Hello World 2nd time!";
+    } else
+    if (c == 3) {
+      cout << "Hi!";
+    } else
+    if (c == 4) {
+      cout << "Hi again!";
+    }
+      
+    return 0;
+}
